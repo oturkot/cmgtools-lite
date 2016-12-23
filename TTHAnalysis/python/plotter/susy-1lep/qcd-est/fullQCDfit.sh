1391b8abd124de728c8bf449aad02d81e40440ad
Basil Schneider <basil.schneider@cern.ch>
2016-12-22 18:51:26 -0600

    Update QCD scripts to new LT bins

--- TTHAnalysis/python/plotter/susy-1lep/qcd-est/fullQCDfit.sh
+++ TTHAnalysis/python/plotter/susy-1lep/qcd-est/fullQCDfit.sh
@@ -3,12 +3,12 @@
 file=$1
 
 verb=0
-lumi=13
+lumi=36.5
 
-./makeQCDtemplateFit.py $file -b -l $lumi -v $verb
-./makeQCDtemplateFit.py $file -b -l $lumi -v $verb --mc
-./makeQCDtemplateFit.py $file -b -l $lumi -v $verb --mc -c
+#./makeQCDtemplateFit.py $file -b -l $lumi -v $verb
+#./makeQCDtemplateFit.py $file -b -l $lumi -v $verb --mc
+#./makeQCDtemplateFit.py $file -b -l $lumi -v $verb --mc -c
 
 ./makeQCDtemplateFit.py $file -b -l $lumi -v $verb -i
-./makeQCDtemplateFit.py $file -b -l $lumi -v $verb -i --mc
-./makeQCDtemplateFit.py $file -b -l $lumi -v $verb -i --mc -c
+#./makeQCDtemplateFit.py $file -b -l $lumi -v $verb -i --mc
+#./makeQCDtemplateFit.py $file -b -l $lumi -v $verb -i --mc -c
