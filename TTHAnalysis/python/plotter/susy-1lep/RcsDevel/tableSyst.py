75c3a785597b6be7f4420fa13576ae72de4d16f4
Basil Schneider <basil.schneider@cern.ch>
2017-02-13 14:34:45 +0100

    Get systematic uncertainties dynamically

--- TTHAnalysis/python/plotter/susy-1lep/RcsDevel/tableSyst.py
+++ TTHAnalysis/python/plotter/susy-1lep/RcsDevel/tableSyst.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys,os
+import glob
 
 #from makeYieldPlots import *
 from makeYieldTables import *
@@ -10,10 +11,15 @@ if __name__ == "__main__":
 
     ## remove '-b' option
 
-    pattern = "Syst"
+    if len(sys.argv) > 1:
+        pattern = sys.argv[1]
+        print '# pattern is', pattern
+    else:
+        print "No pattern given!"
+        exit(0)
 
     #BinMask LTX_HTX_NBX_NJX for canvas names
-    basename = os.path.basename(pattern)
+    basename = os.path.basename('Syst')
     mask = basename.replace("*","X_")
 
     # Define storage
@@ -31,25 +37,32 @@ if __name__ == "__main__":
 #    dlSlopePath = "Yields/systs/dilepSlope/test/merged"; paths.append(dlSlopePath)
 
 
-    tptPath = "YieldsJan15/systs/topPt/MC/allSF_noPU/meth1A/merged/"; paths.append(tptPath)
-    puPath = "YieldsJan15/systs/PU/MC/allSF/meth1A/merged/"; paths.append(puPath)
-    wxsecPath = "YieldsJan15/systs/wXsec/MC/allSF_noPU/meth1A/merged/"; paths.append(wxsecPath)
-    dlConstPath = "YieldsJan15/systs/DLConst/merged"; paths.append(dlConstPath)
-    dlSlopePath = "YieldsJan15/systs/DLSlope/merged"; paths.append(dlSlopePath)
-    #jerPath = "Yields/systs/JER/merged"; paths.append(jerPath)                                                               #jerNoPath = "YieldsJan15/systs/JER_YesNo/merged"; paths.append(jerNoPath)
-    btagPath = "YieldsJan15/systs/btag/hadFlavour/fixXsec/allSF_noPU/meth1A/merged/"; paths.append(btagPath)
-    jecPath = "YieldsJan15/systs/JEC/MC/allSF_noPU/meth1A/merged/"; paths.append(jecPath)
-    wpolPath = "YieldsJan15/systs/Wpol/MC/allSF_noPU/meth1A/merged/"; paths.append(wpolPath)
-    ttvxsecPath = "YieldsJan15/systs/TTVxsec/MC/allSF_noPU/meth1A/merged/"; paths.append(ttvxsecPath)
+    #tptPath = "YieldsJan15/systs/topPt/MC/allSF_noPU/meth1A/merged/"; paths.append(tptPath)
+    #puPath = "YieldsJan15/systs/PU/MC/allSF/meth1A/merged/"; paths.append(puPath)
+    #wxsecPath = "YieldsJan15/systs/wXsec/MC/allSF_noPU/meth1A/merged/"; paths.append(wxsecPath)
+    #dlConstPath = "YieldsJan15/systs/DLConst/merged"; paths.append(dlConstPath)
+    #dlSlopePath = "YieldsJan15/systs/DLSlope/merged"; paths.append(dlSlopePath)
+    ##jerPath = "Yields/systs/JER/merged"; paths.append(jerPath)                                                               #jerNoPath = "YieldsJan15/systs/JER_YesNo/merged"; paths.append(jerNoPath)
+    #btagPath = "YieldsJan15/systs/btag/hadFlavour/fixXsec/allSF_noPU/meth1A/merged/"; paths.append(btagPath)
+    #jecPath = "YieldsJan15/systs/JEC/MC/allSF_noPU/meth1A/merged/"; paths.append(jecPath)
+    #wpolPath = "YieldsJan15/systs/Wpol/MC/allSF_noPU/meth1A/merged/"; paths.append(wpolPath)
+    #ttvxsecPath = "YieldsJan15/systs/TTVxsec/MC/allSF_noPU/meth1A/merged/"; paths.append(ttvxsecPath)
 
     #jecPath = "Yields/systs/JEC/EWK/full/merged/"; paths.append(jecPath)
+
+    # Add all systematics
+    paths = glob.glob('{}/syst_*/merged/'.format(pattern))
 
     for path in paths:
         yds.addFromFiles(path+'/*NJ68*',("lep","sele"))
         yds9.addFromFiles(path+'/*NJ9i*',("lep","sele"))
 
-    systs = ["btagHF","btagLF","Wxsec","Wpol","TTVxsec","topPt","PU","JEC","DLSlope","DLConst"]#,"JEC"]
-    systsprint = ["b-tag HF","b-tag LF","W xsec","W polar","TTV xsec","Top pT","PU","JES","dilep slope","dilep const"]#,"JEC"]
+    #systs = ["btagHF","btagLF","Wxsec","Wpol","TTVxsec","topPt","PU","JEC","DLSlope","DLConst"]#,"JEC"]
+    #systsprint = ["b-tag HF","b-tag LF","W xsec","W polar","TTV xsec","Top pT","PU","JES","dilep slope","dilep const"]#,"JEC"]
+
+    systs = glob.glob('{}/syst_*'.format(pattern))
+    systs = [syst[syst.find('syst_')+5:] for syst in systs]
+    systsprint = systs
 
     # Sample and variable
     samp = "EWK"
