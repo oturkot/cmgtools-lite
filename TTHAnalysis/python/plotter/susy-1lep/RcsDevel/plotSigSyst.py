d39520568c884906a2356fbef74955f7457b54e6
Basil Schneider <basil.schneider@cern.ch>
2017-02-17 12:53:05 +0100

    Add flat 2 % trigger uncertainty

--- TTHAnalysis/python/plotter/susy-1lep/RcsDevel/plotSigSyst.py
+++ TTHAnalysis/python/plotter/susy-1lep/RcsDevel/plotSigSyst.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
         flats["lepSF"] = 0.05
         flats["lumi"] = 0.026
         flats["iso"] = 0.03
-        #flats["trig"] = 0.01
+        flats["trig"] = 0.02
         #flats = {}
 
         for i, syst in enumerate(sorted(flats.keys())):
